72903d4391861b2e6ab731b095d77decb461a410
hiteshchundi <hitzhraj96@gmail.com>
2026-06-25 17:58:47 +0530

    Implemented duplicate job detection and persistent scraping state

--- app/utils/file_handler.py
+++ app/utils/file_handler.py
@@ -13,3 +13,17 @@ def save_jobs_to_json(jobs, filename="jobs.json"):
         )
 
     print(f"\nSaved {len(jobs)} jobs to {filename}")
+
+def load_existing_jobs(filename="jobs.json"):
+
+    try:
+
+        with open(filename, "r", encoding="utf-8") as file:
+
+            jobs = json.load(file)
+
+            return jobs
+
+    except FileNotFoundError:
+
+        return []
--- main.py
+++ main.py
@@ -2,17 +2,37 @@ from app.scraper.linkedin_scraper import search_jobs
 
 from app.utils.file_handler import save_jobs_to_json
 
+from app.utils.file_handler import load_existing_jobs
+
 
 def main():
 
     print("\nStarting AI Job Agent...\n")
 
-    jobs = search_jobs()
+    existing_jobs = load_existing_jobs()
 
-    save_jobs_to_json(jobs)
+    existing_links = {
+        job["link"]
+        for job in existing_jobs
+    }
+
+    scraped_jobs = search_jobs()
+
+    new_jobs = []
+
+    for job in scraped_jobs:
+
+        if job["link"] not in existing_links:
+
+            new_jobs.append(job)
+
+    updated_jobs = existing_jobs + new_jobs
+
+    save_jobs_to_json(updated_jobs)
+
+    print(f"\nFound {len(new_jobs)} new jobs")
 
     print("\nJob scraping completed.\n")
-
 
 if __name__ == "__main__":
     main()
